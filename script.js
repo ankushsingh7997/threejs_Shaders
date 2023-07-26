95ff3930826e79d509d67fc300d517fdee7ebee9
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-07-27 01:57:17 +0530

    optimise

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ document.body.appendChild(renderer.domElement)
 // to create a shape
 const geometry=new THREE.BoxGeometry(1,1,1)
 const material=new THREE.MeshBasicMaterial({color:'red'})
-const cube=new THREE.Mesh(geometry,material);
+// const cube=new THREE.Mesh(geometry,material);
+// if using Line to see in skeleton form
+const cube=new THREE.Line(geometry,material);
+
+
 
 scene.add(cube);
 // cube.position.x=4
@@ -24,25 +28,38 @@ scene.add(cube);
 
 let flag=true;
 
+// function animate()
+// {
+
+//     if(cube.position.x>5) flag=false;
+//     else if(cube.position.x< -5) flag=true
+
+//     if(flag)cube.position.x+=0.1
+//     else cube.position.x-=0.1
+//     // to give rotation to cube
+//     cube.rotation.x+=0.05
+//     cube.rotation.y+=0.05
+//     cube.rotation.z+=0.05
+//     // cube.position.y+=0.1
+//     // cube.position.z+=0.1
+
+
+
+//     renderer.render(scene,camera)
+//     requestAnimationFrame(animate);
+// }
+// // for safety purpose  because sometimes any system is not capable for much smothness
+ 
+// animate()
+// // setInterval(animate,100)
+
+// q represents theta here 
+let q=0
+animate()
 function animate()
 {
+    cube.position.x=5*Math.sin(q+=0.02)
 
-    if(cube.position.x>5) flag=false;
-    else if(cube.position.x< -5) flag=true
-
-    if(flag)cube.position.x+=0.1
-    else cube.position.x-=0.1
-    // to give rotation to cube
-    cube.rotation.x+=0.05
-    cube.rotation.y+=0.05
-    cube.rotation.z+=0.05
-    // cube.position.y+=0.1
-    // cube.position.z+=0.1
     renderer.render(scene,camera)
     requestAnimationFrame(animate);
 }
-// for safety purpose  because sometimes any system is not capable for much smothness
- 
-animate()
-// setInterval(animate,100)
-
