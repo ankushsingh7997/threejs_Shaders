0d1c01a89b931de9dda2e7de7450970c3bad40cb
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-07-27 03:52:09 +0530

    light

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
 
 const scene=new THREE.Scene();
 const camera=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-camera.position.z=5;
+camera.position.z=50;
 // renderer using canvas
 const renderer=new THREE.WebGLRenderer();
 // size for the canvas
 renderer.setSize(window.innerWidth,window.innerHeight)
 // add canvas to body
 document.body.appendChild(renderer.domElement)
+
+// lights
+const ambianceLight=new THREE.AmbientLight(0xffffff,0.3)
+// arguments are color and intensity
+scene.add(ambianceLight)
+
+const pointLight=new THREE.PointLight(0xffffff,1,100)
+// arguments are color intensity and available in distance in units 
+// pointLight.position.set(5,0,1)
+scene.add(pointLight)
+
 // to create a shape
-// const geometry=new THREE.BoxGeometry(1,1,1)
+const geometry=new THREE.BoxGeometry(5,5,5)
 // capsule geometry
 // const geometry=new THREE.CapsuleGeometry(1,1,50,8)
 // const geometry=new THREE.CircleGeometry(2,32)
 // const geometry=new THREE.ConeGeometry(2,2,50)
-const geometry=new THREE.CylinderGeometry(1.5,2,3,100);
-const material=new THREE.MeshBasicMaterial({color:'red'})
-// const cube=new THREE.Mesh(geometry,material);
+// const geometry=new THREE.CylinderGeometry(1.5,2,3,100);
+// const material=new THREE.MeshBasicMaterial({color:'red'})
+// if we want it to react on light the we have to use 
+const material=new THREE.MeshStandardMaterial({color:'red'})
+const cube=new THREE.Mesh(geometry,material);
 // if using Line to see in skeleton form
-const cube=new THREE.Line(geometry,material);
+// const cube=new THREE.Line(geometry,material);
+
+const lightGeometry=new THREE.SphereGeometry(1,12,16)
+const lightmaterial=new THREE.MeshBasicMaterial({color:'white'})
+const lightSphare=new THREE.Mesh(lightGeometry,lightmaterial)
 
 
-
-scene.add(cube);
+scene.add(cube,lightSphare);
 // cube.position.x=4
 // cube.rotation.x=1 // rn this is taking value in radian
 // to use degree we have a formula
@@ -63,10 +79,20 @@ let q=0
 animate()
 function animate()
 {
-    cube.position.x=5*Math.sin(q+=0.02)
-    cube.rotation.x+=0.05
-        cube.rotation.y+=0.05
-        cube.rotation.z+=0.05
+    q+=0.01
+    let qsin=Math.sin(q);
+    let qcos=Math.cos(q);
+    cube.position.x=3*qsin
+
+    let scaledCos=30 *qcos
+    let scaledSin=30 *qsin
+    pointLight.position.set(scaledCos,0,scaledSin)
+     
+    lightSphare.position.set(scaledCos,0,scaledSin)
+
+    cube.rotation.x+=0.01
+        cube.rotation.y+=0.01
+        cube.rotation.z+=0.01
     renderer.render(scene,camera)
     requestAnimationFrame(animate);
 }
