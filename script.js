adaf5cf907f58810747ddd6980db88482186c9b8
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-07-27 01:25:57 +0530

    animation

--- script.js
+++ script.js
@@ -14,5 +14,35 @@ const material=new THREE.MeshBasicMaterial({color:'red'})
 const cube=new THREE.Mesh(geometry,material);
 
 scene.add(cube);
+// cube.position.x=4
+// cube.rotation.x=1 // rn this is taking value in radian
+// to use degree we have a formula
+// cube.rotation.x=Math.PI * (45/180.0)
+// renderer.render(scene,camera)
+// now implementing animation using function
 
-renderer.render(scene,camera)
+
+let flag=true;
+
+function animate()
+{
+
+    if(cube.position.x>5) flag=false;
+    else if(cube.position.x< -5) flag=true
+
+    if(flag)cube.position.x+=0.1
+    else cube.position.x-=0.1
+    // to give rotation to cube
+    cube.rotation.x+=0.05
+    cube.rotation.y+=0.05
+    cube.rotation.z+=0.05
+    // cube.position.y+=0.1
+    // cube.position.z+=0.1
+    renderer.render(scene,camera)
+    requestAnimationFrame(animate);
+}
+// for safety purpose  because sometimes any system is not capable for much smothness
+ 
+animate()
+// setInterval(animate,100)
+
